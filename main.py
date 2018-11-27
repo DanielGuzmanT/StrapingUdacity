0ab02e28d5fc5f261f2972dd9c6c4b78007c296b
DanielGuzmanT <jhairguz.t@gmail.com>
2018-11-27 13:51:06 -0500

    codigo generador de files html por scraping eliminados, ya se obtuvieron manualmente

--- main.py
+++ main.py
@@ -10,11 +10,3 @@ html = list(soup.children)[1]
 # [<class 'bs4.element.Tag'>, <class 'bs4.element.NavigableString'>, <class 'bs4.element.Tag'>]
 body = list(html.children)[2]
 
-file = open("UdacityCourses.html", "w")
-file.write(body.prettify())
-file.close()
-
-ir_course_card_catalog = html.find("ir-course-card-catalog")
-file = open("UdacityCoursesCatalog.html","w")
-file.write(ir_course_card_catalog.prettify())
-file.close()
